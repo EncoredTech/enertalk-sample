f246859ed3e0da18a372e9f5b8fefad0c3a4d054
Sanghyeon-encored <yongdamsh@encoredtech.com>
2016-02-26 00:25:06 +0900

    - card thumbnail image cache applied - card name label added - ETUICardKit updated

--- iOS/ETSampleApp/UICardCollectionViewCell.swift
+++ iOS/ETSampleApp/UICardCollectionViewCell.swift
@@ -13,30 +13,46 @@ class UICardCollectionViewCell: UICollectionViewCell {
     
     // MARK: Properties
     @IBOutlet weak var cardImageView: UIImageView!
+    @IBOutlet weak var cardNameLabel: UILabel!
+    
+    let defaultStorage = NSUserDefaults.standardUserDefaults()
     
     var card: AnyObject? {
         didSet {
-            if let card = card as? ETUICard {
-                if let thumbnailURL = card.thumbnailURL {
-                    NSURLSession.sharedSession().dataTaskWithURL(thumbnailURL, completionHandler: { (data, response, error) -> Void in
-                        
-                        guard
-                            let httpURLResponse = response as? NSHTTPURLResponse where httpURLResponse.statusCode == 200,
-                            let mimeType = response?.MIMEType where mimeType.hasPrefix("image"),
-                            let data = data where error == nil,
-                            let image = UIImage(data: data)
-                            else {
-                                return
-                        }
-                        
-                        dispatch_async(dispatch_get_main_queue()) { () -> Void in
-                            self.cardImageView.image = image
-                        }
-                        
-                    }).resume()
+            
+            guard let card = card as? ETUICard, let thumbnailURL = card.thumbnailURL else {
+                return
+            }
+            
+            cardNameLabel.text = card.name
+            
+            if let imageData = defaultStorage.objectForKey(thumbnailURL.absoluteString) as? NSData {
+                dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                    self.cardImageView.image = UIImage(data: imageData)
                 }
+            } else {
+                
+                NSURLSession.sharedSession().dataTaskWithURL(thumbnailURL, completionHandler: { (data, response, error) -> Void in
+                    
+                    guard
+                        let httpURLResponse = response as? NSHTTPURLResponse where httpURLResponse.statusCode == 200,
+                        let mimeType = response?.MIMEType where mimeType.hasPrefix("image"),
+                        let data = data where error == nil,
+                        let image = UIImage(data: data)
+                        else {
+                            return
+                    }
+                    
+                    self.defaultStorage.setObject(data, forKey: thumbnailURL.absoluteString)
+                    
+                    dispatch_async(dispatch_get_main_queue()) { () -> Void in
+                        self.cardImageView.image = image
+                    }
+                    
+                }).resume()
                 
             }
+
         }
     }
     

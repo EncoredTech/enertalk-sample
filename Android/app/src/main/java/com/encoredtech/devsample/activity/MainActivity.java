a27a79f6f047b778e5d5931ee1be2c044a00874b
namhoon <namhoon@encoredtech.com>
2016-03-26 00:27:41 +0900

    안드로이드 세션 쿠키 삭제

--- Android/app/src/main/java/com/encoredtech/devsample/activity/MainActivity.java
+++ Android/app/src/main/java/com/encoredtech/devsample/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.CookieManager;
 import android.widget.Button;
 
 import com.encoredtech.devsample.R;
@@ -17,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CookieManager cm = CookieManager.getInstance();
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            cm.removeSessionCookies(null);
+        } else {
+            cm.removeSessionCookie();
+        }
 
         Button signBtn = (Button) findViewById(R.id.signBtn);
         signBtn.setOnClickListener(new View.OnClickListener() {
